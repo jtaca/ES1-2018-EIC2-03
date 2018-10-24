12e0e885a701a4f8f5362052952d15bffdb08b57
reavh-iscteiul <reavh@iscte-iul.pt>
2018-10-24 20:34:07 +0100

    Added temporary constructor

--- BomDiaAcademia/src/entry_objects/TwitterEntry.java
+++ BomDiaAcademia/src/entry_objects/TwitterEntry.java
@@ -3,13 +3,30 @@ package entry_objects;
 import java.util.Date;
 
 import other.Service;
+import twitter4j.Status;
 
 /**
  * The Class TwitterEntry.
  */
 public class TwitterEntry implements InformationEntry {
 
-	/* (non-Javadoc)
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Status status;
+
+	public TwitterEntry(Status status) {
+		this.status = status;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see entry_objects.InformationEntry#getDate()
 	 */
 	@Override
@@ -18,7 +35,9 @@ public class TwitterEntry implements InformationEntry {
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see entry_objects.InformationEntry#getService()
 	 */
 	@Override
