205e2ddb0612705f20ae8ca0d8d1b9530f3302a0
ddmso-iscteiul <ddmso@iscte-iul.com>
2018-11-19 21:42:57 +0000

    Added twittertest class to junitTests

---
+++ BomDiaAcademia/src/jUnitTests/TwitterTest.java
@@ -0,0 +1,23 @@
+package jUnitTests;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+import entry_objects.InformationEntry;
+import twitter.TwitterFunctions;
+import twitter4j.Status;
+
+public class TwitterTest {
+
+	@Test
+	public void loggerTestgetKeys(){
+		
+		}
+		
+}
+
+
