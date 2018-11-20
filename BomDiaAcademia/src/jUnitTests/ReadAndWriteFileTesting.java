d3cfca18cf14a140c3bfaa88939b92b17532fcb9
jtaca <jtaca@iscte-iul.pt>
2018-11-20 01:23:51 +0000

    Read and Write XML Updated

---
+++ BomDiaAcademia/src/jUnitTests/ReadAndWriteFileTesting.java
@@ -0,0 +1,29 @@
+package jUnitTests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import other.XMLUserConfiguration;
+
+public class ReadAndWriteFileTesting {
+
+	@Test
+	public void testReadAndWriteFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveListOfInformationEntry() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadListOfInformationEntry() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ BomDiaAcademia/src/jUnitTests/TwitterFunctionsTesting.java
@@ -0,0 +1,44 @@
+package jUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TwitterFunctionsTesting {
+
+	@Test
+	public void testRetweet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRequestTwitter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTweets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTweetsForUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTweetsForUserByDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTweetsForUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetKeys() {
+		fail("Not yet implemented");
+	}
+
+}
