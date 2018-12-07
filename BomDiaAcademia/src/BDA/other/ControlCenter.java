6864aa09f5c2a162bdf4a2b63c83b31623cffc6f
Alexandre Mendes <afcms1111@iscte-iul.pt>
2018-12-07 20:17:45 +0000

    ControlCenter getIgnoreService implementation and add method to ignore service on refresh

--- BomDiaAcademia/src/BDA/other/ControlCenter.java
+++ BomDiaAcademia/src/BDA/other/ControlCenter.java
@@ -10,62 +10,130 @@ import BDA.interfaces.ServiceInstance;
 import BDA.twitter.TwitterConnection;
 import BDA.other.Service;
 
+/**
+ * The Class ControlCenter.
+ */
 public class ControlCenter {
 	
+	/** The email list. */
 	private List<EmailConnection> emailList = null;
+	
+	/** The current email used. */
 	private EmailConnection currentEmailUsed = null;
+	
+	/** The ignore email. */
 	private boolean ignore_email = false;
 	
+	/** The twitter list. */
 	private List<TwitterConnection> twitterList = null;
+	
+	/** The ignore twitter. */
 	private boolean ignore_twitter = false;
 	
+	/** The facebook list. */
 	private List<FacebookConnection> facebookList = null;
+	
+	/** The ignore facebook. */
 	private boolean ignore_facebook = false;
 	
+	/** The Constant INSTANCE. */
 	private static final ControlCenter INSTANCE = new ControlCenter();
 	
+	/**
+	 * Instantiates a new control center.
+	 */
 	private ControlCenter() {
 	}
 	
+	/**
+	 * Gets the single instance of ControlCenter.
+	 *
+	 * @return single instance of ControlCenter
+	 */
 	public static ControlCenter getInstance() {
 		return INSTANCE;
 	}
 	
+	/**
+	 * Gets the email list.
+	 *
+	 * @return the email list
+	 */
 	public List<EmailConnection> getEmailList() {
 		return emailList;
 	}
 	
+	/**
+	 * Gets the current email used.
+	 *
+	 * @return the current email used
+	 */
 	public EmailConnection getCurrentEmailUsed() {
 		return currentEmailUsed;
 	}
 	
 	
+	/**
+	 * Gets the twitter list.
+	 *
+	 * @return the twitter list
+	 */
 	public List<TwitterConnection> getTwitterList() {
 		return twitterList;
 	}
 	
+	/**
+	 * Gets the facebook list.
+	 *
+	 * @return the facebook list
+	 */
 	public List<FacebookConnection> getFacebookList() {
 		return facebookList;
 	}
 	
+	/**
+	 * Sets the email list.
+	 *
+	 * @param emailList the new email list
+	 */
 	public synchronized void setEmailList(List<EmailConnection> emailList) {
 		this.emailList = emailList;
 	}
 	
+	/**
+	 * Sets the current email used.
+	 *
+	 * @param currentEmailUsed the new current email used
+	 */
 	public synchronized void setCurrentEmailUsed(EmailConnection currentEmailUsed) {
 		this.currentEmailUsed = currentEmailUsed;
 	}
 	
 	
+	/**
+	 * Sets the twitter list.
+	 *
+	 * @param twitterList the new twitter list
+	 */
 	public synchronized void setTwitterList(List<TwitterConnection> twitterList) {
 		this.twitterList = twitterList;
 	}
 	
+	/**
+	 * Sets the facebook list.
+	 *
+	 * @param facebookList the new facebook list
+	 */
 	public synchronized void setFacebookList(List<FacebookConnection> facebookList) {
 		this.facebookList = facebookList;
 	}
 	
 	
+	/**
+	 * Adds the email connection to email list.
+	 *
+	 * @param emailConnection the email connection
+	 */
 	public synchronized void addEmailConnectionToEmailList(EmailConnection emailConnection) {
 		if(emailList != null && !emailList.contains(emailConnection)) {
 			try {
@@ -77,6 +145,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Adds the email connections to email list.
+	 *
+	 * @param emailConnections the email connections
+	 */
 	public synchronized void addEmailConnectionsToEmailList(List<EmailConnection> emailConnections) {
 		if(emailList != null) {
 			List<ServiceInstance> to_add = new ArrayList<ServiceInstance>();
@@ -96,6 +169,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Adds the twitter function to twitter list.
+	 *
+	 * @param twitterFunction the twitter function
+	 */
 	public synchronized void addTwitterFunctionToTwitterList(TwitterConnection twitterFunction) {
 		if(twitterList != null && !twitterList.contains(twitterFunction)) {
 			try {
@@ -107,6 +185,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Adds the twitter functions to twitter list.
+	 *
+	 * @param twitterFunctions the twitter functions
+	 */
 	public synchronized void addTwitterFunctionsToTwitterList(List<TwitterConnection> twitterFunctions) {
 		if(twitterList != null) {
 			List<ServiceInstance> to_add = new ArrayList<ServiceInstance>();
@@ -126,6 +209,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Adds the facebook connection to facebook list.
+	 *
+	 * @param facebookConnection the facebook connection
+	 */
 	public synchronized void addFacebookConnectionToFacebookList(FacebookConnection facebookConnection) {
 		if(facebookList != null && !facebookList.contains(facebookConnection)) {
 			try {
@@ -137,6 +225,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Adds the facebook connections to facebook list.
+	 *
+	 * @param facebookConnection the facebook connection
+	 */
 	public synchronized void addFacebookConnectionsToFacebookList(List<FacebookConnection> facebookConnection) {
 		if(facebookList != null) {
 			List<ServiceInstance> to_add = new ArrayList<ServiceInstance>();
@@ -156,6 +249,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Removes the email connection from email list.
+	 *
+	 * @param emailConnection the email connection
+	 */
 	public synchronized void removeEmailConnectionFromEmailList(EmailConnection emailConnection) {
 		if(emailList != null && emailList.contains(emailConnection)) {
 			try {
@@ -167,6 +265,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Removes the email connections from email list.
+	 *
+	 * @param emailConnections the email connections
+	 */
 	public synchronized void removeEmailConnectionsFromEmailList(List<EmailConnection> emailConnections) {
 		if(emailList != null) {
 			List<ServiceInstance> to_remove = new ArrayList<ServiceInstance>();
@@ -186,6 +289,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Removes the twitter function from twitter list.
+	 *
+	 * @param twitterFunction the twitter function
+	 */
 	public synchronized void removeTwitterFunctionFromTwitterList(TwitterConnection twitterFunction) {
 		if(twitterList != null && twitterList.contains(twitterFunction)) {
 			try {
@@ -197,6 +305,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Removes the twitter functions from twitter list.
+	 *
+	 * @param twitterFunctions the twitter functions
+	 */
 	public synchronized void removeTwitterFunctionsFromTwitterList(List<TwitterConnection> twitterFunctions) {
 		if(twitterList != null) {
 			List<ServiceInstance> to_remove = new ArrayList<ServiceInstance>();
@@ -216,6 +329,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Removes the facebook connection from facebook list.
+	 *
+	 * @param facebookConnection the facebook connection
+	 */
 	public synchronized void removeFacebookConnectionFromFacebookList(FacebookConnection facebookConnection) {
 		if(facebookList != null && facebookList.contains(facebookConnection)) {
 			try {
@@ -227,6 +345,11 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Removes the facebook connections from facebook list.
+	 *
+	 * @param facebookConnections the facebook connections
+	 */
 	public synchronized void removeFacebookConnectionsFromFacebookList(List<FacebookConnection> facebookConnections) {
 		if(facebookList != null) {
 			List<ServiceInstance> to_remove = new ArrayList<ServiceInstance>();
@@ -246,6 +369,12 @@ public class ControlCenter {
 		}
 	}
 	
+	/**
+	 * Sets the ignore service.
+	 *
+	 * @param service the service
+	 * @param ignore the ignore
+	 */
 	public synchronized void setIgnoreService(Service service, boolean ignore) {
 		switch (service) {
 			case EMAIL:
@@ -258,6 +387,28 @@ public class ControlCenter {
 				ignore_facebook = ignore;
 				break;
 		}
+	}
+	
+	/**
+	 * Gets the ignore service.
+	 *
+	 * @param service the service
+	 * @return the ignore service
+	 */
+	public boolean getIgnoreService(Service service) {
+		boolean ignore = false;
+		switch (service) {
+			case EMAIL:
+				ignore = ignore_email;
+				break;
+			case TWITTER:
+				ignore = ignore_twitter;
+				break;
+			case FACEBOOK:
+				ignore = ignore_facebook;
+				break;
+		}
+		return ignore;
 	}
 
 }
--- BomDiaAcademia/src/BDA/other/OtherStaticFunction.java
+++ BomDiaAcademia/src/BDA/other/OtherStaticFunction.java
@@ -27,6 +27,10 @@ public class OtherStaticFunction {
 		try {
 			ServiceReadTask task = null;
 			
+			boolean ignore_email = ControlCenter.getInstance().getIgnoreService(Service.EMAIL);
+			boolean ignore_twitter = ControlCenter.getInstance().getIgnoreService(Service.TWITTER);
+			boolean ignore_facebook = ControlCenter.getInstance().getIgnoreService(Service.FACEBOOK);
+			
 			boolean use_info_from_file_to_email = true;
 			boolean use_info_from_file_to_twitter = true;
 			boolean use_info_from_file_to_facebook = true;
@@ -37,19 +41,19 @@ public class OtherStaticFunction {
 			
 			List<ServiceReadTask> tasks = new ArrayList<ServiceReadTask>();
 			
-			if(email_connections != null && !email_connections.isEmpty()) {
+			if(!ignore_email && email_connections != null && !email_connections.isEmpty()) {
 				for(EmailConnection email : email_connections) {
 					tasks.add(new EmailReaderTask(email));
 				}
 				use_info_from_file_to_email = false;
 			}
-			if(twitter_functions != null && !twitter_functions.isEmpty()) {
+			if(!ignore_twitter && twitter_functions != null && !twitter_functions.isEmpty()) {
 				for(TwitterConnection twitter : twitter_functions) {
 					tasks.add(new TwitterPostReaderTask());
 				}
 				use_info_from_file_to_twitter = false;
 			}
-			if(facebook_connections != null && !facebook_connections.isEmpty()) {
+			if(!ignore_facebook && facebook_connections != null && !facebook_connections.isEmpty()) {
 				for(FacebookConnection facebook : facebook_connections) {
 					tasks.add(new FacebookPostReaderTask());
 				}
@@ -61,19 +65,19 @@ public class OtherStaticFunction {
 				for(XMLUserConfiguration xml_user_config : list_of_user_configuration) {
 					switch (xml_user_config.getService()) {
 					case EMAIL:
-						if(use_info_from_file_to_email == true) {
+						if(!ignore_email && use_info_from_file_to_email == true) {
 							task = new EmailReaderTask(new EmailConnection(xml_user_config.getUsername(), xml_user_config.getPassword()));
 						}
 						break;
 						
 					case TWITTER:
-						if(use_info_from_file_to_twitter == true) {
+						if(!ignore_twitter && use_info_from_file_to_twitter == true) {
 							task = new TwitterPostReaderTask(); // Require to be implemented object oriented and not function oriented
 						}
 						break;
 						
 					case FACEBOOK:
-						if(use_info_from_file_to_facebook == true) {
+						if(!ignore_facebook && use_info_from_file_to_facebook == true) {
 							task = new FacebookPostReaderTask(); // Require to be implemented object oriented and not function oriented
 						}
 						break;
